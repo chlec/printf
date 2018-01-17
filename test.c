0bc183e69ee4042bf2f44f2b8c23d3a932103dd2
Charles LECALIER <clecalie@e2r6p4.42.fr>
2018-01-17 13:32:06 +0100

    modif test.c

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: clecalie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 14:20:30 by clecalie          #+#    #+#             */
-/*   Updated: 2018/01/12 14:22:23 by clecalie         ###   ########.fr       */
+/*   Updated: 2018/01/17 13:31:29 by clecalie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,19 @@
 
 int		main(void)
 {
-	//setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "");
 	int e = 7;
-	printf("Length: %d\n", ft_printf("%d %-20S || %-5s%.7s %c |%lx| %20p\n", 234, L"我是一只猫", "haha", "abcdef", 97, 2147483649, &e));
-	printf("Length: %d\n",    printf("%d %-20S || %-5s%.7s %c |%lx| %20p\n", 234, L"我是一只猫", "haha", "abcdef", 97, 2147483649, &e));
-	ft_printf("%--.4u et %#O%#012O%1lc\n", -12, 0, 0, 95);
-	printf("%--.4u et %#O%#012O%1lc\n", -12, 0, 0, 95);
+	printf("Length: %d\n", ft_printf("%d || %-5s%.7s %c |%lx| %20p\n", 234, "haha", "abcdef", 97, 2147483649, &e));
+	printf("Length: %d\n",    printf("%d || %-5s%.7s %c |%lx| %20p\n", 234, "haha", "abcdef", 97, 2147483649, &e));
 	printf("Length 2: %d\n", ft_printf("test %-12i et %--2.4s %24s !\n", 0, NULL, "cooc"));
 	printf("Length 2: %d\n", printf("test %-12i et %--2.4s %24s !\n", 0, NULL, "cooc"));
 	printf("Length 3: %d\n", ft_printf("me/ %10C test\n", L'à'));
 	printf("Length 3: %d\n", printf("em/ %10C test\n", L'à'));
 	printf("Length 4: %d\n", ft_printf("Test/ %s aaaaaaa\n", NULL));
 	printf("Length 4: %d\n", printf("Test/ %s aaaaaaa\n", NULL));
-	printf("Length 5: %d\n", ft_printf("%ld%ld\n", 0, 42));
-	printf("Length 5: %d\n", printf("%ld%ld\n", 0, 42));
-	printf("Length 6: %d\n", ft_printf("%s%p%d%d%p%s%p%p%s\n", "a", &free, 1, 2, &malloc, "b", &free, &malloc, "c"));
-	printf("Length 6: %d\n", printf("%s%p%d%d%p%s%p%p%s\n", "a", &free, 1, 2, &malloc, "b", &free, &malloc, "c"));
+	printf("Length 5: %d\n", ft_printf("%--.4u et %#O%#012O%1lc\n", -12, 0, 0, 95));
+	printf("Length 5: %d\n",   printf("%--.4u et %#O%#012O%1lc\n", -12, 0, 0, 95);
+	ft_printf("%C\n", 0xD800);
+	printf("%C\n", 0xD800);
 	return (0);
 }
