48563897877e1c4ecddc26dbab7529385573cdef
Charles LECALIER <clecalie@e2r6p4.42.fr>
2018-01-22 14:20:21 +0100

    ajout test.c

---
+++ test.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clecalie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/05 14:20:30 by clecalie          #+#    #+#             */
+/*   Updated: 2018/01/22 14:19:16 by clecalie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <locale.h>
+
+int		main(void)
+{
+	setlocale(LC_ALL, "");
+	int e = 7;
+	printf("Length: %d\n", ft_printf("%d || %-5s%.7s %c |%lx| %20p\n", 234, "haha", "abcdef", 97, 2147483649, &e));
+	printf("Length: %d\n",    printf("%d || %-5s%.7s %c |%lx| %20p\n", 234, "haha", "abcdef", 97, 2147483649, &e));
+	printf("Length 2: %d\n", ft_printf("test %-12i et %--2.4s %24s !\n", 0, NULL, "cooc"));
+	printf("Length 2: %d\n", printf("test %-12i et %--2.4s %24s !\n", 0, NULL, "cooc"));
+	printf("Length 3: %d\n", ft_printf("me/ %10C test\n", L'à'));
+	printf("Length 3: %d\n", printf("em/ %10C test\n", L'à'));
+	printf("Length 4: %d\n", ft_printf("Test/ %s aaaaaaa\n", NULL));
+	printf("Length 4: %d\n", printf("Test/ %s aaaaaaa\n", NULL));
+	printf("Length 5: %d\n", ft_printf("%--.4u et %#O%#012O%1lc\n", -12, 0, 0, 95));
+	printf("Length 5: %d\n",   printf("%--.4u et %#O%#012O%1lc\n", -12, 0, 0, 95));
+	ft_printf("%C\n", 800);
+	printf("%C\n", 800);
+	ft_printf("%s\n", "salut");
+	while(1);
+	return (0);
+}
